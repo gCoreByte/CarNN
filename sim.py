922ad5f45694ca5f3a504382ded4833fe04848e0
CoreByte <mhkbyte@gmail.com>
2019-04-12 19:56:55 +0300

    Initial commit, comment, drive function

---
+++ sim.py
@@ -0,0 +1,75 @@
+import math as m
+
+class Car:
+
+    #initialize variables
+
+    def __init__(self, x, y, angle):
+
+        #current pos
+        self.x = x
+        self.y = y
+        #current x, y speed
+        self.xSpeed = 0
+        self.ySpeed = 0
+        #current x, y acceleration
+        self.xAcceleration = 0
+        self.yAcceleration = 0
+        #max, min for acceleration, speed
+        self.maxAcceleration = 10
+        self.maxSpeed = 42069
+        self.minSpeed = -1
+        self.rAcceleration = 0
+        self.accelerationAngle = 0
+        self.angle = angle
+        self.angularSpeed = 0
+        self.maxAngularSpeed = 1
+
+    #drive, movement function
+
+    def drive(self, acceleration, angular):
+
+        #turn
+        self.turn(angular)
+
+        # get new rAcceleration vector
+
+        self.xAcceleration += m.sin(self.accelerationAngle) * acceleration
+        self.yAcceleration += m.cos(self.accelerationAngle) * acceleration
+        #self.rAcceleration += m.sqrt(self.xAcceleration**2 + self.yAcceleration**2)
+        #meil pole ju vaja resultanti??
+
+        # find new speed
+
+        self.xSpeed += self.xAcceleration
+        self.ySpeed += self.yAcceleration
+
+        # find new location
+
+        self.x += self.xSpeed
+        self.y += self.ySpeed
+
+        # checking crash
+
+    #turning, handles angular speed, accelerationAngle
+
+    def turn(self, angular):
+        self.angle += angular
+        self.accelerationAngle = 360
+        while self.accelerationAngle > 90:
+            self.accelerationAngle -= 90
+
+        """
+        angularSpeed = self.angle - desiredAngle
+        if self.angularSpeed > self.maxAngularSpeed:
+            self.angularSpeed = self.maxAngularSpeed
+        elif abs(self.angularSpeed) > self.maxAngularSpeed:
+            self.angularSpeed = -self.maxAngularSpeed
+        self.angle += angularSpeed
+        """
+
+    # checking if we've crashed
+
+    def haveCrashed(self):
+        #todo
+        pass
